ff5b0632a167e7875f0bae6e379b92183f47bac2
Samuel Penaranda <psamumoli12@gmail.com>
2026-04-07 21:31:02 -0500

    add unit test

---
+++ src/test/java/org/model/PlayerPieceTest.java
@@ -0,0 +1,48 @@
+package org.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlayerPieceTest {
+
+    @Test
+    void moveTest_UP() {
+        PlayerPiece playerPiece = new PlayerPiece(new Coordinates(4, 4));
+
+        playerPiece.move(MoveDirection.UP);
+
+        assertEquals(4, playerPiece.getLocation().getX());
+        assertEquals(3, playerPiece.getLocation().getY());
+    }
+
+    @Test
+    void moveTest_DOWN() {
+        PlayerPiece playerPiece = new PlayerPiece(new Coordinates(4, 4));
+
+        playerPiece.move(MoveDirection.DOWN);
+
+        assertEquals(4, playerPiece.getLocation().getX());
+        assertEquals(5, playerPiece.getLocation().getY());
+    }
+
+    @Test
+    void moveTest_LEFT() {
+        PlayerPiece playerPiece = new PlayerPiece(new Coordinates(4, 4));
+
+        playerPiece.move(MoveDirection.LEFT);
+
+        assertEquals(3, playerPiece.getLocation().getX());
+        assertEquals(4, playerPiece.getLocation().getY());
+    }
+
+    @Test
+    void moveTest_RIGHT() {
+        PlayerPiece playerPiece = new PlayerPiece(new Coordinates(4, 4));
+
+        playerPiece.move(MoveDirection.RIGHT);
+
+        assertEquals(5, playerPiece.getLocation().getX());
+        assertEquals(4, playerPiece.getLocation().getY());
+    }
+}
